663d66e15348906707f72afd5365442f0cca51a1
colly-m <sindanicolly@gmail.com>
2024-01-04 14:30:07 -0800

    Perfect forest update

--- binary_trees.h
+++ binary_trees.h
@@ -49,6 +49,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t calculate_power(size_t base, size_t exponent);
+size_t count_nodes(const binary_tree_t *tree);
+size_t calculate_tree_height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
