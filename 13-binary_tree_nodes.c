0d3caca484feefd4709790262b9c11f82478235e
colly-m <sindanicolly@gmail.com>
2024-01-04 12:08:23 -0800

    Nodes 13 update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,12 +9,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count = 0;
+
 	if (!tree)
 	{
 		return (0);
 	}
-
-	size_t count = 0;
 
 	count += (tree->left || tree->right) ? 1 : 0;
 	count += binary_tree_nodes(tree->left);
