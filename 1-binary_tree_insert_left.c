e4fc0f77e18d3f4766885ef3e6a97af97268b4eb
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-01-04 07:22:43 -0800

    fixfiles

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,24 +13,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-if (!parent)
-return (NULL);
+	if (!parent)
+	return (NULL);
 
-new_node = malloc(sizeof(binary_tree_t));
-if (!new_node)
-return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	return (NULL);
 
-new_node->n = value;
-new_node->parent = parent;
-new_node->right = NULL;
-new_node->left = parent->left;
-parent->left = new_node;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
+	new_node->left = parent->left;
+	parent->left = new_node;
 
-if (new_node->left)
-new_node->left->parent = new_node;
+	if (new_node->left)
+	new_node->left->parent = new_node;
 
-return (new_node);
+	return (new_node);
 }
 
