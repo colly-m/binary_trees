111e7884c21d529733b1061286fc7d7146134632
colly-m <sindanicolly@gmail.com>
2024-01-04 08:16:58 -0800

    Our forest update

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,5 @@
+#ifndef BINARY_TREES_H
+
 #define BINARY_TREES_H
 
 #include <stdlib.h>
@@ -8,11 +10,12 @@
 #include <unistd.h>
 
 /**
- * Binary tree node: holds an int, links to parent/children
- * @n: Node value (int)
- * @parent: Parent node (optional)
- * @left: Left child node (optional)
- * @right: Right child node (optional) Binary tree node
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
  */
 struct binary_tree_s
 {
@@ -50,7 +53,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-/* Highlighting the specific nature of the task */
+/* Advanced tasks */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
@@ -73,7 +76,7 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-/* Emphasizing their practical purpose */
+/* helper functions */
 int _pow_recursion(int x, int y);
 binary_tree_t *bta_helper(binary_tree_t *root, const binary_tree_t *first,
 			  const binary_tree_t *second);
