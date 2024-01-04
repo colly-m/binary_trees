598edca1a175210456f3fad8d5a0a5583eba857b
colly-m <sindanicolly@gmail.com>
2024-01-04 11:55:26 -0800

    Depth update fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,17 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t d = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	size_t depth = 0;
 
 	while (tree->parent)
 	{
-		depth++;
+		d++;
 		tree = tree->parent;
 	}
-	return (depth);
+	return (d);
 }
