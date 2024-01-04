ca4d6b955776c53d5895aea16085070bff9fabef
colly-m <sindanicolly@gmail.com>
2024-01-04 12:03:34 -0800

    leave update fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,13 +8,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left_leaves;
+	size_t right_leaves;
+
 	if (!tree)
 	{
 		return (0);
 	}
 
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
